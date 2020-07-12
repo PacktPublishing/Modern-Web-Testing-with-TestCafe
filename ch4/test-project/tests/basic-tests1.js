07782d0f33fba14ab9e1b077cd9c1c1ec8f832bd
Marketionist <marketionist@gmail.com>
2020-07-12 10:29:58 -0400

    Add basic-tests 1 and 2

---
+++ ch4/test-project/tests/basic-tests1.js
@@ -0,0 +1,8 @@
+const { Selector } = require('testcafe');
+
+fixture('My first set of tests')
+    .page('http://demo.redmine.org/');
+
+test('My first test', async (t) => {
+    // Your test code
+});
